a2627b12a5962cb150241ac5c9da9d31ad989133
Adeel Feroz <adil.feroz.7@gmail.com>
2023-10-19 00:59:50 +0500

    Time: 27 ms (32.27%), Space: 43.8 MB (93.04%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -4,19 +4,17 @@ class Solution {
         for(int i=0;i<piles.length;i++){
             maxi = Math.max(maxi,piles[i]);
         }
-        int minPossible = maxi;
         int start = 1;
         int end = maxi;
         while(start<=end){
             int mid = (start+end)/2;
             if(calculateHours(piles,mid)<=h){
-                minPossible = Math.min(mid,minPossible);
                 end = mid-1;
             }else{
                 start = mid+1;
             }
         }
-        return minPossible;
+        return start;
     }
 
     public static int calculateHours(int [] arr, int hours){
