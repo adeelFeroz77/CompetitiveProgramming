9e558d15a94d7e7e7d62475556c77452b8b0bebd
Adeel Feroz <adil.feroz.7@gmail.com>
2024-09-12 02:12:55 +0500

    Time: 0 ms (100%), Space: 40.8 MB (10.08%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.java
@@ -2,7 +2,6 @@ class Solution {
     public int minBitFlips(int start, int goal) {
         int diff = start^goal;
         int ans = 0;
-        System.out.println(diff);
         while(diff!=0){
             ans += diff&1;
             diff>>=1;
