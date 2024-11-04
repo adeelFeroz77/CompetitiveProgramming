3558449a8a296e3e2fc2c69204d0b73828ddd642
Adeel Feroz <adil.feroz.7@gmail.com>
2024-11-05 01:35:43 +0500

    Time: 37 ms (23.78%), Space: 46.7 MB (30.3%) - LeetHub

---
+++ 3451-string-compression-iii/3451-string-compression-iii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String compressedString(String word) {
+        int n = word.length();
+        char prev = word.charAt(0);
+        int count = 1;
+        StringBuilder sb = new StringBuilder();
+        for(int i=1; i<n;i ++){
+            char c = word.charAt(i);
+            if(c == prev && count < 9){
+                count++;
+                continue;
+            }
+            sb.append(count + "" + prev);
+            prev = c;
+            count = 1;
+        }
+        sb.append(count + "" + prev);
+        return sb.toString();
+
+    }
+}
