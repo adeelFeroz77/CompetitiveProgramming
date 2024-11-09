1760a39c1b0853c9b89ed88513f9ca44db69022f
Adeel Feroz <adil.feroz.7@gmail.com>
2024-11-09 15:53:17 +0500

    Time: 1735 ms (17.86%), Space: 40.7 MB (71.15%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.java
+++ 3394-minimum-array-end/3394-minimum-array-end.java
@@ -2,8 +2,7 @@ class Solution {
     public long minEnd(int n, int x) {
         long lastNum = x;
         for(long i=1; i<n; i++){
-            lastNum++;
-            lastNum |= x;
+            lastNum = (lastNum +1) | x; 
         }
         return lastNum;
     }
