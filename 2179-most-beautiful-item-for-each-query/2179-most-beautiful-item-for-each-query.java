4243324c2c0baac0e79d92228532b6f58b225e12
Adeel Feroz <adil.feroz.7@gmail.com>
2024-11-12 23:38:52 +0500

    Time: 77 ms (7.25%), Space: 79.5 MB (91.12%) - LeetHub

---
+++ 2179-most-beautiful-item-for-each-query/2179-most-beautiful-item-for-each-query.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] maximumBeauty(int[][] items, int[] queries) {
+        int n = items.length;
+        Arrays.sort(items, (a,b) -> Integer.compare(a[0], b[0]));
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        map.put(items[0][0], items[0][1]);
+        int last = items[0][1];
+        for(int i=1; i<n; i++){
+            int key = items[i][0];
+            int val = items[i][1];
+            if(last > val){
+                map.put(key, last);
+            }else{
+                map.put(key,val);
+                last = val;
+            }
+        }
+        int [] ans = new int[queries.length];
+        for(int i=0; i<queries.length; i++){
+            Integer key = map.floorKey(queries[i]);
+            if(key == null){
+                ans[i] = 0;
+            }else{
+                ans[i] = map.get(key);
+            }
+        }
+        return ans;
+    }
+}
