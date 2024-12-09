71a01d040225675d5f2286a0e140a3a62e259fe3
Adeel Feroz <adil.feroz.7@gmail.com>
2024-12-09 23:10:58 +0500

    Time: 18 ms (15.57%), Space: 109.1 MB (15.1%) - LeetHub

---
+++ 3427-special-array-ii/3427-special-array-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean[] isArraySpecial(int[] nums, int[][] queries) {
+        int n = nums.length;
+        int [] arr = new int[n];
+        int val = 0;
+        for(int i=1; i<n; i++){
+            if(nums[i]%2 == nums[i-1]%2){
+                val++;
+            }
+            arr[i] = val;
+        }
+        boolean [] ans = new boolean[queries.length];
+        System.out.println(Arrays.toString(arr));
+        for(int i=0; i<queries.length; i++){
+            ans[i] = arr[queries[i][0]] == arr[queries[i][1]];
+        }
+        return ans;
+    }
+}
