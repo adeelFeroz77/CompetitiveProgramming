ec10f4ffef2ae4e97bbbd6b9ac84d8d27030a350
Adeel Feroz <adil.feroz.7@gmail.com>
2025-02-05 12:39:48 +0500

    Time: 0 ms (100%), Space: 41.7 MB (39.72%) - LeetHub

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        int n = s1.length();
+        Map<Character, Character> map = new HashMap<>();
+
+        int u = -1;
+        int v = -1;
+
+        boolean isReplaced = false;
+        for(int i=0; i<n; i++){
+            if(s1.charAt(i) != s2.charAt(i)){
+                if(isReplaced){
+                    return false;
+                }
+
+                if (u == -1){
+                    u = s1.charAt(i) - '0';
+                    v = s2.charAt(i) - '0';
+                }else{
+                    if(v != s1.charAt(i)-'0' || u != s2.charAt(i)-'0'){
+                        return false;
+                    }
+                    isReplaced = true;
+                }
+            }
+        }
+        if(u != -1 && !isReplaced){
+            return false;
+        }
+
+        return true;
+    }
+}
