ca872c41fb39767ee22ea73149ceb698455017f0
Adeel Feroz <adil.feroz.7@gmail.com>
2025-10-16 02:12:30 +0500

    Time: 14 ms (96.54%), Space: 88.4 MB (54.98%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
@@ -1,32 +1,21 @@
 class Solution {
     public int maxIncreasingSubarrays(List<Integer> nums) {
         int n = nums.size();
-        int left = 0, right = n/2 + 1;
-        int ans = 1;
-        while(left < right) {
-            int k = (left + right) >> 1;
-            if(increasingSubArrays(nums, k)) {
-                left = k+1;
-                ans = k;
-            } else {
-                right = k;
-            }
-        }
-        return ans;
-    }
+        int maxi = 0, prev = 0, curr = 1;
 
-    public boolean increasingSubArrays(List<Integer> nums, int k) {
-        if (k == 1) return true; 
-        for (int i = 0, j = k, max = 1; j< nums.size()-1; i++, j++) {
-            if (nums.get(i) < nums.get(i + 1) && nums.get(j) < nums.get(j + 1)) {
-                max++;
-                if (max == k) {
-                    return true;
-                }
+        for(int i=1; i<n; i++) {
+            if(nums.get(i) > nums.get(i-1)) {
+                curr++;
             } else {
-                max = 1;
+                // 2 options, either divide current in half
+                // or get the min or curr and prev
+
+                maxi = Math.max(maxi, Math.max(curr/2, Math.min(prev, curr)));
+                prev = curr;
+                curr = 1;
             }
         }
-        return false;
-    }  
+        maxi = Math.max(maxi, Math.max(curr/2, Math.min(prev, curr)));
+        return maxi;
+    }
 }
